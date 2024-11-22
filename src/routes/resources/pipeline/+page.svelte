f8b03d9ea397289d85b1f2c2ae5c6f39c1473b60
sparrowsl <benthorpe.dev@gmail.com>
2024-11-22 07:39:47 +0000

    move steps into separatejs file

--- src/routes/resources/pipeline/+page.svelte
+++ src/routes/resources/pipeline/+page.svelte
@@ -1,51 +1,7 @@
 <script>
   import Journey from '../../../lib/Journey.svelte';
   import Step from '../../../lib/Step.svelte';
-
-  const steps = [
-    {
-      number: 1,
-      title: 'Create Account',
-      description:
-        'Join the platform by creating an account to begin your journey as a creator, donor, or contributor. This is your gateway to submitting projects, tracking progress, and engaging with the community.',
-    },
-    {
-      number: 2,
-      title: 'Submit project',
-      description:
-        'Submit your project idea through our easy-to-use interface. Share your vision, goals, and how it aligns with the DPG mission to get it in front of potential donors and contributors.',
-    },
-    {
-      number: 3,
-      title: 'Receive funding or Contribution',
-      description:
-        'Once your project is live, you can receive funding from donors or contributions from experts in areas like design, engineering, or research to help you grow and succeed.',
-    },
-    {
-      number: 4,
-      title: 'Engage Community',
-      description:
-        'Collaborate with a global network of creators, contributors, and supporters. Keep everyone engaged with regular updates and discussions to strengthen your project',
-    },
-    {
-      number: 5,
-      title: 'Track Milestones and the 9 DPG Standards Checklist',
-      description:
-        'Monitor your progress and ensure your project meets the 9 key standards of a Digital Public Good. Easily track milestones and keep your community informed.',
-    },
-    {
-      number: 6,
-      title: 'Project Meets Requirements and Qualifies as a DPG',
-      description:
-        'Once your project meets all the necessary requirements, it will officially be recognized as a Digital Public Good, opening doors for even more support and impact.',
-    },
-    {
-      number: 7,
-      title: 'Project completes',
-      description:
-        'Your project has reached completion! With the backing of the community and compliance with DPG standards, your project can now make a lasting, global impact.',
-    },
-  ];
+  import { steps } from './pipelineSteps.js';
 </script>
 
 <div
