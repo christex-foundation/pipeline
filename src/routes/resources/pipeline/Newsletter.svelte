e26eb0980bcc91a38c03e70e994226096b9c74ef
sparrowsl <benthorpe.dev@gmail.com>
2024-11-22 09:20:53 +0000

    redesign banner

---
+++ src/routes/resources/pipeline/Newsletter.svelte
@@ -0,0 +1,20 @@
+<div class="p-6 bg-lime-100 rounded-3xl">
+  <h2 class="mb-3 text-xl font-semibold">Stay updated with our newsletter</h2>
+  <!-- TODO: add action if we connect it to an endpoint -->
+  <form class="flex items-center max-md:flex-col max-md:gap-3">
+    <label for="email">
+      <input
+        type="email"
+        id="email"
+        placeholder="johndoe@gmail.com"
+        class="border bg-white border-cyan-950/30 rounded-l-full min-w-full max-sm:rounded-full px-4 py-2 text-sm bg-transparent outline-none text-cyan-950"
+      />
+    </label>
+    <button
+      type="submit"
+      class="px-4 py-2 text-white bg-teal-900 rounded-r-full max-sm:rounded-full max-sm:w-full text-sm"
+    >
+      Sign up
+    </button>
+  </form>
+</div>
