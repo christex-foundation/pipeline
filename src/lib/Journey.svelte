5210bf3d9a19c821b88a85344b31c310371ca8e7
sparrowsl <benthorpe.dev@gmail.com>
2024-11-22 11:15:43 +0000

    fix: move design into Journey component, fixes build crashes

---
+++ src/lib/Journey.svelte
@@ -0,0 +1,51 @@
+<script>
+  import Newsletter from '../routes/resources/pipeline/Newsletter.svelte';
+</script>
+
+<section class="max-w-6xl mx-auto mt-36">
+  <div
+    class="grid max-sm:gap-10 md:grid-cols-2 items-center py-10 px-5 md:px-14 bg-lime-300 max-md:rounded-3xl rounded-[3rem]"
+  >
+    <div class="font-semibold max-w-sm">
+      <h3 class="text-5xl text-teal-950 mb-5">Start your DPG journey now</h3>
+      <p class="text-lg md:text-xl text-teal-800">
+        Discover, support, and create Digital Public Goods on Pipeline.
+      </p>
+    </div>
+
+    <div class="flex flex-col gap-5">
+      <!-- Learn about pipeline -->
+      <div class="flex items-center justify-between p-5 bg-lime-100 rounded-3xl">
+        <div class="flex flex-col gap-1 min-w-fit">
+          <h2 class="text-2xl font-semibold">Learn about Pipeline</h2>
+          <p class="font-light">Get to know about the community</p>
+        </div>
+        <a href="/">
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/b57dd1b95e7392c3d06e9edef7c8e0583a52ef5efb38bdd9d4b163b315b64c06?placeholderIfAbsent=true&apiKey=567aaefef2da4f73a3149c6bc21f1ea8"
+            alt="upper right arrow"
+          />
+        </a>
+      </div>
+
+      <!-- Learn about DPG -->
+      <div class="flex items-center justify-between p-5 bg-lime-100 rounded-3xl">
+        <div class="flex flex-col gap-1 min-w-fit">
+          <h2 class="text-2xl font-semibold">Learn about DPGs</h2>
+          <p class="font-light max-sm:max-w-[15rem]">Get to know about Digital Public Goods</p>
+        </div>
+        <a href="/">
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/b57dd1b95e7392c3d06e9edef7c8e0583a52ef5efb38bdd9d4b163b315b64c06?placeholderIfAbsent=true&apiKey=567aaefef2da4f73a3149c6bc21f1ea8"
+            alt="upper right arrow"
+          />
+        </a>
+      </div>
+
+      <!-- leaving it as a separate component since we might need to use it on another page -->
+      <Newsletter />
+    </div>
+  </div>
+</section>
--- src/routes/resources/pipeline/+page.svelte
+++ src/routes/resources/pipeline/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import { steps } from './pipelineSteps.js';
   import illustration from './how.png';
-  import Newsletter from './Newsletter.svelte';
+  import Journey from '$lib/Journey.svelte';
 </script>
 
 <div class="h-40 md:h-[45dvh] bg-[#d1ea9a]/70 mb-20 grid place-content-center">
@@ -28,51 +28,5 @@
     {/each}
   </section>
 
-  <section class="max-w-6xl mx-auto mt-36">
-    <div
-      class="grid max-sm:gap-10 md:grid-cols-2 items-center py-10 px-5 md:px-14 bg-lime-300 max-md:rounded-3xl rounded-[3rem]"
-    >
-      <div class="font-semibold max-w-sm">
-        <h3 class="text-5xl text-teal-950 mb-5">Start your DPG journey now</h3>
-        <p class="text-lg md:text-xl text-teal-800">
-          Discover, support, and create Digital Public Goods on Pipeline.
-        </p>
-      </div>
-
-      <div class="flex flex-col gap-5">
-        <!-- Learn about pipeline -->
-        <div class="flex items-center justify-between p-5 bg-lime-100 rounded-3xl">
-          <div class="flex flex-col gap-1 min-w-fit">
-            <h2 class="text-2xl font-semibold">Learn about Pipeline</h2>
-            <p class="font-light">Get to know about the community</p>
-          </div>
-          <a href="/">
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/b57dd1b95e7392c3d06e9edef7c8e0583a52ef5efb38bdd9d4b163b315b64c06?placeholderIfAbsent=true&apiKey=567aaefef2da4f73a3149c6bc21f1ea8"
-              alt="upper right arrow"
-            />
-          </a>
-        </div>
-
-        <!-- Learn about DPG -->
-        <div class="flex items-center justify-between p-5 bg-lime-100 rounded-3xl">
-          <div class="flex flex-col gap-1 min-w-fit">
-            <h2 class="text-2xl font-semibold">Learn about DPGs</h2>
-            <p class="font-light max-sm:max-w-[15rem]">Get to know about Digital Public Goods</p>
-          </div>
-          <a href="/">
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/b57dd1b95e7392c3d06e9edef7c8e0583a52ef5efb38bdd9d4b163b315b64c06?placeholderIfAbsent=true&apiKey=567aaefef2da4f73a3149c6bc21f1ea8"
-              alt="upper right arrow"
-            />
-          </a>
-        </div>
-
-        <!-- leaving it as a separate component since we might need to use it on another page -->
-        <Newsletter />
-      </div>
-    </div>
-  </section>
+  <Journey />
 </main>
