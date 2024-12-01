cca85b29a43ce09ae9c2fe5d338f550efc936078
edwxrdbobb <edwardbobkamara@gmail.com>
2024-12-01 17:01:28 +0000

    Added back buttons to signup and login for easy navigation

--- src/routes/(auth)/sign-in/+page.svelte
+++ src/routes/(auth)/sign-in/+page.svelte
@@ -1,16 +1,34 @@
 <script>
+  import { goto } from '$app/navigation';
   import { enhance } from '$app/forms';
   import Logo from '$lib/Logo.svelte';
+  import { ArrowLeft } from 'lucide-svelte';
 
   let loading = false;
+  function goBack() {
+    history.back();
+  }
+
 </script>
 
-<section class="flex items-center justify-center basis-full ">
-  
+<section
+  class="flex flex-col items-center justify-center w-full max-w-[1235px] max-md:px-5 max-md:mt-10 mt-20"
+>
+  <div class="w-[60%] max-md:w-[90%] mx-auto mb-6">
+    <a
+    href="#"
+    on:click|preventDefault={goBack}
+    class="flex items-center justify-center text-teal-900 hover:text-white hover:bg-teal-800 border-2 border-teal-800 transition-colors w-[150px] py-4 rounded-full"
+  >
+    <ArrowLeft class="w-5 h-5 mr-2" />
+    <span class="font-medium">Back</span>
+  </a>
+  </div>
+
   <form method="POST" use:enhance class="w-[60%] max-md:w-[80%]">
     <div class="hidden mb-10 max-md:block">
-    <Logo/>
-  </div>
+      <Logo />
+    </div>
     <h2 class="mb-2 text-4xl font-semibold">Sign in</h2>
     <p class="mb-8 opacity-50">Enter your info to sign in</p>
     <div class="flex flex-col gap-2 font-medium">
@@ -37,8 +55,8 @@
 
     <div class="flex flex-wrap items-center justify-between w-full gap-6 mt-6 text-sm leading-none">
       Don't have an account?
-            <a href="/sign-up" class="font-semibold text-neutral-400 hover:text-[#0b383c]">Sign Up</a>
-          </div>
+      <a href="/sign-up" class="font-semibold text-neutral-400 hover:text-[#0b383c]">Sign Up</a>
+    </div>
 
     <button
       type="submit"
@@ -47,7 +65,6 @@
     >
       {loading ? 'Signing in...' : 'Sign in'}
     </button>
-    
 
     <div class="flex flex-wrap items-center justify-between w-full gap-6 mt-6 text-sm leading-none">
       <label class="flex items-center gap-2 font-medium text-black">
@@ -56,6 +73,5 @@
       </label>
       <a href="#forgot-password" class="font-semibold text-neutral-400">Forgot Password?</a>
     </div>
-
   </form>
 </section>
