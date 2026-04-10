e10bdfb7d02d5c7a906cfe580ecf6cc559ceb0fb
Ben <bnjox@users.noreply.github.com>
2026-04-10 13:48:56 +0000

    docs: update more notes on the sql schema

--- db/schema/add_on_delete_constraints.sql
+++ db/schema/add_on_delete_constraints.sql
@@ -1,3 +1,4 @@
+-- User-owned rows
 ALTER TABLE projects
 DROP CONSTRAINT projects_user_id_fkey,
 ADD CONSTRAINT projects_user_id_fkey
@@ -60,3 +61,60 @@ ADD CONSTRAINT project_update_comment_user_id_fkey
 FOREIGN KEY (user_id)
 REFERENCES auth.users(id)
 ON DELETE SET NULL;
+
+-- Project-owned rows
+ALTER TABLE bookmark_project
+DROP CONSTRAINT bookmark_project_project_id_fkey,
+ADD CONSTRAINT bookmark_project_project_id_fkey
+FOREIGN KEY (project_id)
+REFERENCES projects(id)
+ON DELETE CASCADE;
+
+ALTER TABLE category_project
+DROP CONSTRAINT category_project_project_id_fkey,
+ADD CONSTRAINT category_project_project_id_fkey
+FOREIGN KEY (project_id)
+REFERENCES projects(id)
+ON DELETE CASCADE;
+
+ALTER TABLE project_dpg_status
+DROP CONSTRAINT project_dpg_status_project_id_fkey,
+ADD CONSTRAINT project_dpg_status_project_id_fkey
+FOREIGN KEY (project_id)
+REFERENCES projects(id)
+ON DELETE CASCADE;
+
+ALTER TABLE project_members
+DROP CONSTRAINT project_members_project_id_fkey,
+ADD CONSTRAINT project_members_project_id_fkey
+FOREIGN KEY (project_id)
+REFERENCES projects(id)
+ON DELETE CASCADE;
+
+ALTER TABLE project_resource
+DROP CONSTRAINT project_resource_project_id_fkey,
+ADD CONSTRAINT project_resource_project_id_fkey
+FOREIGN KEY (project_id)
+REFERENCES projects(id)
+ON DELETE CASCADE;
+
+ALTER TABLE project_updates
+DROP CONSTRAINT project_updates_project_id_fkey,
+ADD CONSTRAINT project_updates_project_id_fkey
+FOREIGN KEY (project_id)
+REFERENCES projects(id)
+ON DELETE CASCADE;
+
+ALTER TABLE project_update_comment
+DROP CONSTRAINT project_update_comment_project_id_fkey,
+ADD CONSTRAINT project_update_comment_project_id_fkey
+FOREIGN KEY (project_id)
+REFERENCES projects(id)
+ON DELETE CASCADE;
+
+ALTER TABLE project_update_comment
+DROP CONSTRAINT project_update_comment_update_id_fkey,
+ADD CONSTRAINT project_update_comment_update_id_fkey
+FOREIGN KEY (update_id)
+REFERENCES project_updates(id)
+ON DELETE CASCADE;
